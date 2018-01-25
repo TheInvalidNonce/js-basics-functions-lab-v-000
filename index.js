56e05dddee442fec2a594a55fd1d9a24b63f76ed
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-25 19:52:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function calculatesFarePrice(start, destination) {
       return 0;
     case distance > 400 && distance < 2000:
       return distance * 0.02;
-    case distance > 2000:
+    case distance > 2000 && dis:
       return 25;
     case distance > 2500:
       return 'cannot travel that far';

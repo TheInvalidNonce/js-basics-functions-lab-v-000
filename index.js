b63433c73c71b130a53e7d55b51c8cb58d905d83
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-25 19:27:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function calculateVertical(origin, dest) {
   return (dest - origin) * 264
 }
 function distanceFromHqInBlocks(blocks) {
-  return Math.abcs
+  return Math.abs(blocks - 42)
 }
 
 function distanceFromHqInFeet() {
